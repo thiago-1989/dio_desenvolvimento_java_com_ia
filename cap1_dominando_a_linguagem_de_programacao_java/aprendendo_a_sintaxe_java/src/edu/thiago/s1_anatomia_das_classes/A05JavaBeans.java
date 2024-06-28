dd7666c2c54fb59d0c332847901f3dc66a60b2a6
Thiago Oliveira <thiagodevoliveira@hotmail.com>
2024-06-27 21:16:02 -0300

    Java Beans

--- cap1_dominando_a_linguagem_de_programacao_java/aprendendo_a_sintaxe_java/src/edu/thiago/s1_anatomia_das_classes/A05JavaBeans.java
+++ cap1_dominando_a_linguagem_de_programacao_java/aprendendo_a_sintaxe_java/src/edu/thiago/s1_anatomia_das_classes/A05JavaBeans.java
@@ -1,13 +1,41 @@
-/* Conveção 
- * 
- * Variaveis
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
+/*
+ * Umas das maiores dificuldades na programação é escrever algoritmos legíveis a níveis que sejam compreendidos por todo seu time ou por você mesmo no futuro. Para isso a linguagem Java sugere, através de convenções, formas de escrita universal para nossas classes, atributos, métodos e pacotes.
+
+**Variáveis
+
+Mais cedo já aprendemos algumas regras de declaração de variáveis, mas agora iremos conhecer algumas sugestões de de nomenclatura:
+
+Uma variável deve ser clara, sem abreviações ou definição sem sentido;
+Uma variável é sempre no singular, exceto quando se referir a um array ou coleção.
+Defina um idioma único para suas variáveis. Se você for declarar variáveis em inglês, defina todas em inglês.
+
+**Não recomendado
+
+double salMedio = 1500.23  //variável abreviada, o que dificulta a compreensão
+String emails = "aluno@escola.com" //confuso se a variável seria um array ou único e-mail
+String myName = "JOSEPH" //se idioma pt-BR, o valor poder ser de outro idioma mas o nome da variável não 
+
+**Recomendado
+
+double salarioMedio=1500.23;
+String email ="aluno@escola.com";
+String [] emails = {"aluno@escola.com","professor@escola.com"}
+String meuNome = "JOSEPH" 
+
+**Métodos
+
+Os métodos deverão ser nomeados como verbos, através de uma mistura de letras minúsculas e maiúsculas. Em princípio todas as letras que compõem o nome devem ser mantidas em minúsculo, com exceção da primeira letra de cada palavra composta a partir da segunda palavra.
+
+Exemplos sugeridos para nomenclatura de métodos:
+
+somar(int n1, int n2){}
+
+abrirConexao(){}
+
+concluirProcessamento() {}
+
+findById(int id){} // não se assuste, você verá muito método em inglês em sua jornada
+
+calcularImprimir(){} // há algo de errado neste método, ele deveria ter uma única finalidade
+
  */
