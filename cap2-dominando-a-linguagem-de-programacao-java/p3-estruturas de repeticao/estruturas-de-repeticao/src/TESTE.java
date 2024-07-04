b9efaaad56fb9247b06270da84eb5c6366792a5a
Thiago Oliveira <thiagodevoliveira@hotmail.com>
2024-07-04 00:59:12 -0300

    Tratamento de excessões

--- cap2-dominando-a-linguagem-de-programacao-java/p3-estruturas de repeticao/estruturas-de-repeticao/src/TESTE.java
+++
@@ -1,8 +0,0 @@
-public class TESTE {
-    public static void main(String[] args) {
-        for(int x=1;x<=10;x++){
-            if(x % 2==0) //% operador que representa módulo de uma divisão
-                System.out.println(x);
-        }
-    }
-}
